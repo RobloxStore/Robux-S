868e276d1515985627581f22a0a2b63459917b4c
RobloxStore <164667980+RobloxStore@users.noreply.github.com>
2024-03-24 00:20:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,2 @@
-// script.js
-
-// عند تحميل الصفحة
-window.onload = function() {
-    // أضف حدثًا لنموذج تسجيل الدخول
-    const loginForm = document.querySelector('.login-form');
-    loginForm.addEventListener('submit', function(event) {
-        event.preventDefault(); // منع إعادة تحميل الصفحة
-        
-        // احصل على قيم البريد الإلكتروني وكلمة المرور
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
-        
-        // يمكنك هنا إجراء عملية تحقق من بيانات تسجيل الدخول، مثل إرسالها إلى خادم أو التحقق منها بشكل محلي
-        // هذا مجرد مثال بسيط
-        if (email === 'user@example.com' && password === 'password') {
-            alert('تم تسجيل الدخول بنجاح!');
-        } else {
-            alert('فشل تسجيل الدخول! الرجاء التحقق من بيانات الدخول.');
-        }
-    });
-};
+// عرض عدد العملات
+document.getElementById('currency-count').textContent = localStorage.getItem('robuxCount') || 0;
